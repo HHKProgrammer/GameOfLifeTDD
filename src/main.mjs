0226da5bcc82d7c45fb3ee33fa57fc5c855a143c
HHKProgrammer <helin@DESKTOP-AE9GKSP>
2025-04-12 16:53:04 +0300

    save result in file

--- src/main.mjs
+++ src/main.mjs
@@ -1,15 +1,16 @@
-import {parseRLE, nextGeneration} from "./game-of-life.mjs";
+import {parseRLE, nextGeneration, gridToRLE} from "./game-of-life.mjs";
 import fs from 'fs';
 
-const[,, filePath, generationsStr] = process.argv;
+const[,, filePath, generationsStr, outputPath] = process.argv;
 
 if(!filePath || !generationsStr){
-  console.error("node src/main <file.rle> <generations> ");
+  console.error("Usage: node src/main <file.rle> <generations> <outputPath>");
   process.exit(1);
 }
 const generations = parseInt(generationsStr);
 const rleText = fs.readFileSync(filePath, 'utf-8');
 let grid = parseRLE(rleText);
+
 function padGrid(grid, padding = 1){
   const width = grid[0].length;
   const emptyRow = Array(width + padding * 2).fill(false);
@@ -35,5 +36,9 @@ grid = padGrid(grid, 1);
 for(let i = 0; i < generations; i++){
   grid = nextGeneration(grid);
 }
+const rleOutput =gridToRLE(grid);
+fs.writeFileSync(outputPath, rleOutput, 'utf-8');
+
+console.log(`wrote result tp path ${outputPath}`);
 //console.log("debug raw grid=", grid);
 console.log(grid.map(row => row.map(cell => (cell === true ? 'O' : '.')).join('')).join('\n'));
